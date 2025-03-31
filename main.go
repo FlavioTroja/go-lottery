27162aec1fb629e1d77c4c416e1a5977299923be
Flavio Troia <flavio.troia@overzoom.it>
2025-03-31 11:39:12 +0200

    fix: check errors

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 type Studente struct {
 	Nome string
@@ -10,6 +13,17 @@ type Studente struct {
 
 func (s Studente) Saluta() string { 
 	return "Ciao ciao " + s.Nome
+}
+
+func somma(a int, b int) int {
+	return a + b
+}
+
+func divisione (a, b float64) (float64, error) {
+	if b == 0 {
+		return 0, errors.New("divisione per zero")
+	}
+	return a/b, nil
 }
 
 func main() {
@@ -23,4 +37,20 @@ func main() {
 	saluto := s.Saluta()
 
 	fmt.Println(saluto)
+	fmt.Println("Età:", s.Età)
+    fmt.Println("Punteggio:", s.Punteggio)	
+
+	fmt.Println("Risultato somma: ", somma( 5, 3 ))
+
+	div, err := divisione(4, 2)
+	if err != nil {
+		fmt.Println("Errore divisione: ", err)
+	} else {
+		fmt.Println("Risultato divisione: ", div)
+	}
+
+	_, err = divisione(4, 0)
+	if err != nil {
+		fmt.Println("Errore divisione: ", err)
+	} 
 }
